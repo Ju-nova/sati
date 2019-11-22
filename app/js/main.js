dcb7cc812dd238f665061efb3d7c3a8b2533ec22
Yulia <46112108+Ju-nova@users.noreply.github.com>
2019-11-22 11:21:35 +0300

    блок с видео

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,24 @@
 $(document).ready(function(){
 
+// ПОЯВЛЕНИЕ ОТЗЫВОВ ПО КЛИКУ
 $('.review__button').click(function() {
 	  $('.review__block.hidden').removeClass('hidden');
 	  $(this).addClass('hidden');
 });
 
+// СЛАЙДЕР С ВИДЕО
+$('.video-slider').slick({
+
+  slidesToShow: 1,
+  arrows:true,
+  infinite:true,
+  autoplay:false
+  
+});
+$('.video-slider').css({
+       overflow: 'visible',
+       opacity: '1',
+       height: 'auto'
+     });
 });
 
