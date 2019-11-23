ff0c8c24dadd923b8f9653064603ef6584f1e1f7
Yulia <46112108+Ju-nova@users.noreply.github.com>
2019-11-23 16:23:58 +0300

    r

--- app/js/main.js
+++ app/js/main.js
@@ -20,5 +20,38 @@ $('.video-slider').css({
        opacity: '1',
        height: 'auto'
      });
+
+// СПОЙЛЕРЫ
+$( '.faq__question').click(
+  function() {
+    $(this).next('.faq__answer').slideToggle(500);
+
+    $(this).toggleClass('faq-rotate');
+
+  }, 
+);
+// МОДАЛКИ
+    $('.simplebox').simplebox({
+        overlay: {
+            color: 'rgba(0, 0, 0, 0.8)',
+            opacity: 0.7
+        },
+        onOpen: function(obj){console.log('onOpen', obj);},
+        afterOpen: function(obj){console.log('afterOpen', obj);},
+        onClose: function(obj){console.log('onClose', obj);},
+        afterClose: function(obj){console.log('afterClose', obj);}
+    });
+//при закрытии модалки останавливать и видео
+$('.close').click(function() {
+
+   location.reload();
+
+});
+$('.simplebox-overlay').click(function() {
+
+   location.reload();
+
+});  
+
 });
 
