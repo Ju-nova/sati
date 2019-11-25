74b23d9b391cc445ef37da2eef7a0ff60ed9248e
Yulia <46112108+Ju-nova@users.noreply.github.com>
2019-11-25 18:03:33 +0300

    11

--- app/js/main.js
+++ app/js/main.js
@@ -1,52 +1,12 @@
 $(document).ready(function(){
-function checkWidth() {
-    var windowWidth = $('body').innerWidth(),
-        elem = $(".hide"); // лучше сохранять объект в переменную, многократно чтобы не насиловать 
-                                    // страницу для поиска нужного элемента
-    if(windowWidth < 426){
-      
-      // $('.review__slide.hidden').removeClass('hidden'); 
-      $('.hide').addClass('hidden'); 
-
-    }
-  }
-  checkWidth(); // проверит при загрузке страницы
- $(window).resize(function(){
-    checkWidth(); // проверит при изменении размера окна клиента
-  });
-// ПОЯВЛЕНИЕ ОТЗЫВОВ ПО КЛИКУ
-$('.review__button').click(function() {
-
-	  $('.review__slide.hidden').removeClass('hidden');
-    $('.review__block.hidden').removeClass('hidden');
-	  $(this).addClass('hidden');
-});
-
-// СЛАЙДЕР С ВИДЕО
-$('.video-slider').slick({
-
-  slidesToShow: 1,
-  arrows:true,
-  infinite:true,
-  autoplay:false
-  
-});
-$('.video-slider').css({
-       overflow: 'visible',
-       opacity: '1',
-       height: 'auto'
-
-     });
-iframe.setAttribute("allowfullscreen",'');
-// СЛАЙДЕР С ОТЗЫВАМИ
 // function checkWidth() {
 //     var windowWidth = $('body').innerWidth(),
-//         elem = $(".review__slide"); // лучше сохранять объект в переменную, многократно чтобы не насиловать 
+//         elem = $(".hide"); // лучше сохранять объект в переменную, многократно чтобы не насиловать 
 //                                     // страницу для поиска нужного элемента
 //     if(windowWidth < 426){
-//       elem.unwrap(); 
+      
 //       // $('.review__slide.hidden').removeClass('hidden'); 
-//       $('.review__box').addClass('review-slider'); 
+//       $('.hide').addClass('hidden'); 
 
 //     }
 //   }
@@ -54,37 +14,7 @@ iframe.setAttribute("allowfullscreen",'');
 //  $(window).resize(function(){
 //     checkWidth(); // проверит при изменении размера окна клиента
 //   });
-// $('.review-slider').slick({
 
-//   slidesToShow: 1,
-//   arrows:true,
-//   infinite:true,
-//   autoplay:false,
-//   adaptiveHeight:true
-
-  
-// });
-// $('.review-slider').css({
-//        overflow: 'visible',
-//        opacity: '1',
-//        height: 'auto'
-//      });
-
-// var slider4 = $('.sl-rb');
-//   $('.sl-count__total__main').text( slider4.slick("getSlick").slideCount);
-//   $(".sl-rb").on('afterChange', function(event, slick, currentSlide){
-//        $(".sl-count__num__main").text(currentSlide + 1);
-//   });
-
-    
-// СПОЙЛЕРЫ
-$( '.faq__question').click(
-  function() {
-    $(this).next('.faq__answer').slideToggle(500);
-    $(this).toggleClass('faq-rotate');
-
-  }
-);
 // МОДАЛКИ
     $('.simplebox').simplebox({
         overlay: {
@@ -106,7 +36,87 @@ $('.simplebox-overlay').click(function() {
 
    location.reload();
 
-});  
+}); 
+
+// СПОЙЛЕРЫ
+
+$( '.faq__question').click(
+  function() {
+    $(this).next('.faq__answer').slideToggle(500);
+    $(this).toggleClass('faq-rotate');
+
+  }
+);
+// ПОЯВЛЕНИЕ ОТЗЫВОВ ПО КЛИКУ
+$('.review__button').click(function() {
+
+	  // $('.review__slide.hidden').removeClass('hidden');
+   //  $('.review__block.hidden').removeClass('hidden');
+     $('.review').css({
+       height: 'auto',
+       overflow: 'visible'
+     });
+    $('.review__slide.opacity').removeClass('opacity');
+	  $(this).addClass('hidden');
+});
+
+// СЛАЙДЕР С ВИДЕО
+$('.video-slider').slick({
+
+  slidesToShow: 1,
+  arrows:true,
+  infinite:true,
+  autoplay:false
+  
+});
+$('.video-slider').css({
+       overflow: 'visible',
+       opacity: '1',
+       height: 'auto'
+
+     });
+
+// СЛАЙДЕР С ОТЗЫВАМИ
+function checkWidth() {
+    var windowWidth = $('body').innerWidth(),
+        elem = $(".review__slide"); // лучше сохранять объект в переменную, многократно чтобы не насиловать 
+                                    // страницу для поиска нужного элемента
+    if(windowWidth < 426){
+      elem.unwrap(); 
+       $('.review__slide.opacity').removeClass('opacity'); 
+      $('.review__box').addClass('review-slider'); 
+
+    }
+  }
+  checkWidth(); // проверит при загрузке страницы
+ $(window).resize(function(){
+    checkWidth(); // проверит при изменении размера окна клиента
+  });
+$('.review-slider').slick({
+
+  slidesToShow: 1,
+  arrows:true,
+  infinite:true,
+  autoplay:false,
+  adaptiveHeight:true
+
+  
+});
+$('.review-slider').css({
+       overflow: 'visible',
+       opacity: '1',
+       height: 'auto'
+     });
+
+var slider4 = $('.sl-rb');
+  $('.sl-count__total__main').text( slider4.slick("getSlick").slideCount);
+  $(".sl-rb").on('afterChange', function(event, slick, currentSlide){
+       $(".sl-count__num__main").text(currentSlide + 1);
+  });
+
+    
+
+ 
 
 });
 
