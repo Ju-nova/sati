19b42a432eafa693de87772047ce16ff619c64b5
Yulia <46112108+Ju-nova@users.noreply.github.com>
2020-05-11 15:37:27 +0300

    11.05

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,22 @@
 
 
 $(document).ready(function(){
-
-   var navMain = $('.main-nav');
-  var  navToggle = $('.main-nav__toggle');
+// ОСНОВНАЯ НАВИГАЦИЯ САЙТА (ВЕРХНЕЕ МЕНЮ)
+  var navMain    = $('.main-nav');
+  var navToggle  = $('.main-nav__toggle');
 
  $(navToggle).on('click', function() {
  $(navMain).toggleClass('main-nav--closed'); 
-$('body').toggleClass('hidden');
-});
+ $('body').toggleClass('hidden');
+ });
 
-   var subItem = $('.with-submenu');
+  var subItem = $('.with-submenu');
   
  $(subItem).on('click', function() {
   
   var ThisSubMenu = $(this).find('.site-nav__submenu');
-  var subMenu = $('.site-nav__submenu').not(ThisSubMenu);
-  console.log('кнопка');
+  var subMenu     = $('.site-nav__submenu').not(ThisSubMenu);
+
  $(subMenu).removeClass('open'); 
  $(ThisSubMenu).toggleClass('open');
  $(this).toggleClass('open');
@@ -25,41 +25,67 @@ $('body').toggleClass('hidden');
 });
  $('.site-nav__list-item a').on('click', function(event) {
  event.stopPropagation();
-         
-        });
-
+         });
+ 
+// ГЛАВНЫЙ СЛАЙДЕР САЙТА
 
 $('.main-slider').slick({
   dots:true,
 });
+// При загрузке страницs слайдер не будет растягиваться по высоте
+$('.main-slider__slide').css({
+     overflow: 'visible',
+     opacity: '1'
+   });
 
+// ПЕРЕКЛЮЧЕНИЕ БЛОКОВ В FAQ
 $('#btn-check').click(function(){
-        var radio = $('input[name="radio"]:checked').data('id');
+      var radio = $('input[name="radio"]:checked').data('id');
       var level = "#" + radio;
-  // event.preventDefault();
   if ($('input[name="radio"]').is(':checked')){
 
  $('.faq__main').css('display','none');
    $(level).css('display','block');
-   // $('body').css("background-color", "red");
+
   } else {
 
    $(level).addClass('class');
   }
-  //   if ($('input[name="radio1"]').is(':checked')){
-  //   alert('Включен');
-  // } else {
-  //   alert('Выключен');
-  // }
+
 });
-//       var radioCheck = $('input[name="radio"]:checked').data('id');
-//       var levelCheck = "#" + radioCheck;
-// var btne = $(levelCheck).find('.btn');
-$('.btn-return').on('click',(function() {
- // $(levelCheck).css('display','none'); 
+
+$('.btn-return').on('click',(function() { 
  $(this).parent().css('display','none');
   $('.faq__main').css('display','block');
 }));
 
+// МОДАЛЬНЫЕ ОКНА
+    var link = $('.simplebox');
+     
+      $(link).click(function() {
+        event.preventDefault();
+        var modal = $(this).attr('href');
+      $(modal).fadeIn(300);
+        $('body').css({
+      overflow: 'hidden'
+    });
+  });    
+  $('.modal-container').click(function(e) {
+         $(this).fadeOut(300);
+         $('body').css({
+      overflow: 'visible'
+    });
+    // location.reload();
+     console.log('parent function');
+     })
+    .on('click','div', function(e) { 
+       e.stopPropagation();       
+   });
+    $('.close').click(function() {
+  $(".modal-container").fadeOut(300);
+$('body').css({
+      overflow: 'visible'
+    });
+});
 
 })
